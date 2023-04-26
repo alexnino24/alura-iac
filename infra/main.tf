97e3a58c1adc43664d6ea8d0a18a4b36fd1db92c
alex <avdiniz@latam.stefanini.com>
2023-04-25 22:01:57 -0300

    load balancer

--- infra/main.tf
+++ infra/main.tf
@@ -31,7 +31,7 @@ resource "aws_key_pair" "chaveSSH" {
 }
 
 resource "aws_autoscaling_group" "grupo" {
-  availability_zones = ["${var.regiao_aws}a"]
+  availability_zones = ["${var.regiao_aws}a" , "${var.regiao_aws}b"]
   name = var.nomeGrupo
   max_size = var.maximo
   min_size = var.minimo
