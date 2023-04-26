d57a2a0b9968d4cd2abe2d6ee5951508c627c946
alex <avdiniz@latam.stefanini.com>
2023-04-25 22:05:18 -0300

    aprimorando load balancer

--- infra/main.tf
+++ infra/main.tf
@@ -41,3 +41,11 @@ resource "aws_autoscaling_group" "grupo" {
    }
   
 }
+resource "aws_default_subnet" "subnet_1" {
+  availability_zone = "${var.regiao_aws}a"
+  
+}
+resource "aws_default_subnet" "subnet_2" {
+  availability_zone = "${var.regiao_aws}b"
+  
+}
